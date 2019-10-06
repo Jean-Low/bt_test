d58a82d1f5410cc9f3510c5e5dac9ed67be51826
Hugo Mendes <hugospm@al.insper.edu.br>
2019-10-06 16:49:30 -0300

    Update behavior.py

--- Backtesting/behavior.py
+++ Backtesting/behavior.py
@@ -1,22 +1,24 @@
-
 import os
+import sys
 from backtesting import MarketData, evaluateMult, evaluateTick
 
-
+sys.path.append("./models")
+sys.path.append("./strategies")
 
 strategy_files = []
 # r=root, d=directories, f = files
 for r, d, f in os.walk("./strategies"):
     for file in f:
         if file[-3:] == ".py":
-            strategy_files.append(file[:-3]) 
+            strategy_files.append(file[:-3])
 
 sm_relations = {}
 with open("relations.txt", "r") as file:
     line = file.readline()
     while line:
         relat = line.strip('\n').split(':')
-        sm_relations[relat[0]] = relat[1]
+        print(relat)
+        sm_relations[relat[0]] = [relat[1].split("-")[0], relat[1].split(" ")[0]]
         line = file.readline()
 
 print(sm_relations)
@@ -28,8 +30,8 @@ for f in strategy_files:
     print("sou um " + f)
     print(type(f))
     exec("from strategies.{0} import {0}".format(f))
-    exec("strategy_list.append({0}(sm_relations['{0}']))".format(f))
-    
+    exec("strategy_list.append({0}(*sm_relations['{0}']))".format(f))
+
 
 #type_list = [MarketData.HIST, MarketData.TICK]
 #file_list = [{'IBOV':'^BVSP.csv'}, {'PETR4':'2018-03-07.csv' }]
